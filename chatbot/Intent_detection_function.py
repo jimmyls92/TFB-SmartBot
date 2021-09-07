86a7ae68f19d1894cfaaec30251565579a0a11e4
Jaime Lopez <jaimelopezsahuquillo@gmail.com>
2021-09-07 13:30:07 +0200

    Funcion chatbot bastante avanzada pero da error (siempre calcula las mismas probabilidades para los intents y por tanto sale todo el rato el mismo intent)

--- chatbot/Intent_detection_function.py
+++ chatbot/Intent_detection_function.py
@@ -1,17 +1,7 @@
-import random 
-
-import joblib
-
-import sys
-
-
-#from utils import load_cinema_reviews
-
 import random
 random.seed(42)
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plot
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
@@ -19,75 +9,63 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.feature_selection import chi2
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, precision_recall_curve
-
-import matplotlib.pyplot as plt
-
-import random 
-
 import joblib
-
+import keras
 import sys
 
-
-#from utils import load_cinema_reviews
-
-import random
-random.seed(42)
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plot
-import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.feature_selection import chi2
-from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
-from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, precision_recall_curve
-
 import matplotlib.pyplot as plt
 
+import entity_function
+
+from entity_function import getEntities
 
 
-df = pd.read_excel(r"C:\Users\jimmy\Desktop\Copia_de_Seguridad\Keepcoding_2\TFB-SmartBot\chatbot\training_chatbot.xlsx")
-
-finish = False
-while finish == False:
-    keyboard = input('\n')
-    
-    intent, keyword = Intent_model(keyboard)
-
-    if intent == "Gretting":
-        print("Hi what can I do for you?")
-
-    elif intent =="Search":
-        print("This is what I have found on {}".format(keyword))
-    
-    elif intent=="Suggestions":
-        print("This is what I have found related to {}".format(keyword))
 
 
-def chatbot():
-    input_text = input()
 
-    test = pd.DataFrame(data={'Sentence': [input_text]})
-    df_test_proc, test_proc = processing(test, cv=cv)
+def Intent_detection_function(keyboard):
+    mlp_greeting = keras.models.load_model('model_greetings.h5')
+    mlp_search = keras.models.load_model('model_search.h5')
+    mlp_suggestion = keras.models.load_model('model_suggestion.h5')
+    mlp_farewell = keras.models.load_model('model_farewell.h5')
+    cv = joblib.load("vectorizer.pkl")
 
-    gret_prob = mlp_greeting.predict_proba(test_proc)
-    search_prob = mlp_search.predict_proba(test_proc)
-    sugg_prob = mlp_suggestion.predict_proba(test_proc)
 
-    probs = [gret_prob, search_prob, sugg_prob]
+    input_text = pd.DataFrame(data={'Sentence': [keyboard]})
+    print(input_text)
+    test_proc = cv.transform(input_text)
+    print(test_proc)
+    #df_test_proc, test_proc = processing(test, cv=cv)
+
+    gret_prob = mlp_greeting.predict(test_proc)
+    search_prob = mlp_search.predict(test_proc)
+    sugg_prob = mlp_suggestion.predict(test_proc)
+    fare_prob = mlp_farewell.predict(test_proc)
+
+
+    probs = [gret_prob, search_prob, sugg_prob, fare_prob]
+    print(probs)
     idx = np.argmax(probs)
 
     if idx == 0:
-        print("Esto es un saludo")
+        intent = "Greeting"
+        keyword = None
     elif idx == 1:
-        print("Esto es una búsqueda")
-    else:
-        print("Esto es una sugerencia")
+        intent = "Search"
+        keyword = getEntities(input_text)
+    elif idx == 2:
+        intent = "Suggestions"
+        keyword = getEntities(input_text)
+    elif idx == 3:
+        intent = "Farewell"
+        keyword = None
 
+    return intent, keyword
+
+
+
+
+'''
     print('¿Hemos acertado?')
 
     respuesta = input()
@@ -112,5 +90,5 @@ def chatbot():
                 print("Esto es una búsqueda")
     else:
         print('¡Genial! ¡Hemos acertado!')
-
+'''
 
--- chatbot/chatbot_main.py
+++ chatbot/chatbot_main.py
@@ -38,35 +38,69 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plot
 import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.feature_selection import chi2
-from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
-from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, precision_recall_curve
+
 
 import matplotlib.pyplot as plt
 
-import chatbot_function
+import warnings
+warnings.filterwarnings("ignore")
+
+from Intent_detection_function import Intent_detection_function
+import wikipedia as wiki
 
 
 
 finish = False
+activated= False
 while finish == False:
-    print("Hi, I am wikibot, what can I do for you?")
-    keyboard = input('\n')
-    
-    intent, keyword = chatbot_function(keyboard)
 
-    if intent == "Gretting":
-        print("Hi what can I do for you?")
+    if activated == False:
+        print("Please, say a gretting to activate Wikibot")
 
-    elif intent =="Search":
-        print("This is what I have found on {}".format(keyword))
+        keyboard = input('')
     
-    elif intent=="Suggestions":
-        print("This is what I have found related to {}".format(keyword))
+        intent, keyword = Intent_detection_function(keyboard)
+
+        if intent == "Greeting":
+            activated = True
+            print("Hi, this is Wikibot, an NLP based chatbot aimed to help in your wikipedia search")
+
+
+    if activated == True:
+
+        while activated == True:
+            print("What can I do for you?")
+            keyboard = input('')
+            intent, keyword = Intent_detection_function(keyboard)
+
+            if intent == "Greeting":
+                print("Wikibot is already activated, try to ask me a question!")
+
+            elif intent == "Search":
+                try:
+                    #print("He encontrado: .{}".format(wiki.search(keyboard, results)))
+                    print(wiki.summary(keyword))
+                except:
+                    print("\nI have not found any coincidence. \n")
+                    if wiki.suggest(keyword) != None:
+                        print("Did you mean {} \n".format(wiki.suggest(keyword)))
+                    else:
+                        print("Try again")
+
+            elif intent == "Suggestions":
+                print("This is what Wikibot has found related to {}:".format(keyword))
+                print("{}".format((wiki.suggest(keyword))))
+
+            elif intent == "Farewell":
+                print("It was a pleasure helping you. Wikibot is now going to sleep!")
+                activated = False
+
+
+    if activated == False:
+        print("That is not a greeting, try again if you want to activate Wikibot")
+        continue
+
 
 
 
