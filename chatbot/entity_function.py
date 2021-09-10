ba5bfc1e71c11853822870b4cc16f2f8ac0e1d55
kike <emonereolopez@gmail.com>
2021-09-10 14:48:27 +0200

    resuelto un bug en entity_function por el que no devolvia bien el nombre de la entidad

--- chatbot/entity_function.py
+++ chatbot/entity_function.py
@@ -2,14 +2,15 @@
 """
 Created on Sun Sep  5 18:36:48 2021
 
-@author: luism
+@author: luism, Jaime
 """
 
 import spacy
 def getEntities(text):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
-    entities_list = [(X.text, X.label_) for X in doc.ents]
+    entities_list = [(X.text) for X in doc.ents]
+    print(entities_list)
     if len(entities_list)>1:
         # devuelve 1 si hay más de una entidad
         entity = 1
