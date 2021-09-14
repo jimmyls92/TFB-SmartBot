e05f34cf94cdd91ca40abb6d1e576b397b8397ca
begonapermar <88267980+begonapermar@users.noreply.github.com>
2021-09-14 20:21:36 +0200

    Update with the last model

--- chatbot/chatbot_main_modified.py
+++ chatbot/chatbot_main_modified.py
@@ -1,6 +1,30 @@
-from Intent_detection_function import Intent_detection_function
-import wikipedia as wiki
+import random 
+
+import joblib
+
+import sys
+
+import random
+random.seed(42)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plot
+import sklearn
 import requests
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_selection import chi2
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, precision_recall_curve
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from  Intent_detection_2_0 import Intent_detection_function
+import wikipedia as wiki
 
 # This is a sample Python script.
 
@@ -9,21 +33,24 @@ import requests
 finish = False
 activated = False
 
+df = pd.read_excel('database_intents.xlsx')
+
 def generate_text(keyboard):
     global finish 
     global activated
+    print(1)
     url = "https://96xuaw9m48.execute-api.us-east-2.amazonaws.com/test2/transaction?topic="
     payload = {}
     headers = {
         'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAP7YTAEAAAAAE4h6IXYWi%2BGMp9HwZjpkzxmVzyQ%3D9t7woenbmjW9ACLilj4zh4Vd7o1acUPwjJ10zL46ebYS9EZKGT'
     }
     if not finish and not activated:
-        intent, keyword = Intent_detection_function(keyboard)
+        intent, keyword = Intent_detection_function(keyboard, df)
         if intent == "Greeting":
             activated = True
             return "Hi, this is Wikibot, an NLP based chatbot aimed to help in your wikipedia search.\n What can I do for you?"
     if activated:
-        intent, keyword = Intent_detection_function(keyboard)
+        intent, keyword = Intent_detection_function(keyboard, df)
         if intent == "Greeting":
             return "Wikibot is already activated, try to ask me a question!"
         elif intent == "Search":
